5eb3d720a37245969a3a912993719a0e69694a56
jhimy <u201818218@upc.edu.pe>
2022-09-30 18:14:33 -0500

    dwwd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CRUD-j';
+  employeeArray: Employee[] = [
+    { id: 1, name: 'Jhimy', country: 'USA' },
+    { id: 2, name: 'angelica', country: 'USA' },
+    { id: 3, name: 'jou', country: 'USA' },
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+  }
+  delete() {
+    if (confirm('Esta seguro?')) {
+      this.employeeArray = this.employeeArray.filter(
+        (x) => x != this.selectedEmployee
+      );
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
